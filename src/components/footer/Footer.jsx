4fc2de276b1ba1bb6d88cbb25da5a877fdca23f5
Hilola22 <hilolaorazaliyeva2203@gmail.com>
2025-09-05 17:27:50 +0500

    hero

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
     <footer className="bg-slate-900 text-gray-300 py-6">
       <div className="container mx-auto flex flex-col items-center justify-between gap-4">
         <p className="text-xs text-gray-400">
-          © {new Date().getFullYear()} MyWebsite. All rights reserved.
+           MyWebsite. FakeApi Informations.
         </p>
       </div>
     </footer>
